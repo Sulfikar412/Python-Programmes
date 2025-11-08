c0d0045dceff191dd0914b7757b0ad554ebb8aff
Sulfikar412 <ssulfi6235@gmail.com>
2025-11-08 19:28:03 +0530

    dictionary ceated

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+personal_information={
+    "Name":"Sulfikar.s",
+    "Age":23,
+    "place":"Konni",
+    "date_of_birth":21-10-2002,
+    "E-mail":"ssulfi6235@gmail.com",
+}
+for x,y in personal_information.items():
+    print(x,":",y)
